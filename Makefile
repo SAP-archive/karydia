0fb9b166ee0554cb61b26f156fbab26461a820f9
Michael Schubert <michael@kinvolk.io>
2018-10-23 17:47:53 +0200

    First CLI skeleton

---
+++ Makefile
@@ -0,0 +1,10 @@
+VERSION=$(shell git describe --tags --always --dirty)
+
+.PHONY: all
+all: build
+
+.PHONY: build
+build:
+	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bin/karydia \
+		-ldflags "-s -X github.com/kinvolk/karydia/cli/cmd.version=$(VERSION)" \
+		cli/main.go
---
+++ cli/cmd/root.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+var rootCmd = &cobra.Command{
+	Use:   "karydia",
+	Short: "karydia - Kubernetes security walnut",
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.karydia.yaml)")
+
+	rootCmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
+		return viper.BindPFlags(cmd.Flags())
+	}
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".karydia")
+	}
+	viper.SetEnvPrefix("KARYDIA")
+	viper.AutomaticEnv()
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
