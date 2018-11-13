c81caf701320c4d54bf4731cd16d5f6cb046c7b5
Michael Schubert <michael@kinvolk.io>
2018-11-13 18:04:21 +0100

    pkg/server: log http requests info

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -48,7 +48,7 @@ func New(config *Config, webhook *webhook.Webhook) (*Server, error) {
 
 	httpServer := &http.Server{
 		Addr:      config.Addr,
-		Handler:   mux,
+		Handler:   server.middlewareLogger(mux),
 		TLSConfig: config.TLSConfig,
 	}
 
@@ -69,4 +69,26 @@ func (s *Server) Shutdown(ctx context.Context) error {
 
 func (s *Server) handlerHealthz(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
+}
+
+type responseWriter struct {
+	http.ResponseWriter
+	status int
+}
+
+func (w *responseWriter) WriteHeader(status int) {
+	w.status = status
+	w.ResponseWriter.WriteHeader(status)
+}
+
+func (s *Server) middlewareLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		rw := &responseWriter{w, http.StatusOK}
+		next.ServeHTTP(rw, r)
+		s.logger.WithFields(logrus.Fields{
+			"remote_addr": r.RemoteAddr,
+			"method":      r.Method,
+			"status":      rw.status,
+		}).Infof("%s", r.URL)
+	})
 }
