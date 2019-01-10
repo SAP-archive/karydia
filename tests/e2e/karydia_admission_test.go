3a31d6cfa596711f1b73e306acc83eb27206c2c6
Michael Schubert <michael@kinvolk.io>
2019-01-10 13:06:40 +0100

    tests: add `TestAutomountServiceAccountTokenDefaultFalse`

--- tests/e2e/karydia_admission_test.go
+++ tests/e2e/karydia_admission_test.go
@@ -1,6 +1,7 @@
 package e2e
 
 import (
+	"strings"
 	"testing"
 	"time"
 
@@ -66,5 +67,48 @@ func TestAutomountServiceAccountToken(t *testing.T) {
 	timeout := 2 * time.Minute
 	if err := f.WaitPodRunning(pod.ObjectMeta.Namespace, pod.ObjectMeta.Name, timeout); err != nil {
 		t.Fatalf("pod never reached state running")
+	}
+}
+
+func TestAutomountServiceAccountTokenDefaultFalse(t *testing.T) {
+	namespace, err := f.CreateTestNamespace()
+	if err != nil {
+		t.Fatalf("failed to create test namespace: %v", err)
+	}
+
+	ns := namespace.ObjectMeta.Name
+
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "karydia-e2e-test-pod",
+			Namespace: ns,
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:  "nginx",
+					Image: "nginx",
+				},
+			},
+		},
+	}
+
+	pod, err = f.KubeClientset.CoreV1().Pods(ns).Create(pod)
+	if err != nil {
+		t.Fatalf("failed to create pod: %v", err)
+	}
+
+	timeout := 2 * time.Minute
+	if err := f.WaitPodRunning(pod.ObjectMeta.Namespace, pod.ObjectMeta.Name, timeout); err != nil {
+		t.Fatalf("pod never reached state running")
+	}
+
+	if pod.Spec.AutomountServiceAccountToken == nil || *pod.Spec.AutomountServiceAccountToken {
+		t.Fatalf("pod's `automountServiceAccountToken` hasn't been set to `false` by default")
+	}
+	for _, vol := range pod.Spec.Volumes {
+		if strings.HasPrefix(vol.Name, "default-token-") {
+			t.Fatalf("pod has a default service account token mounted when it should not")
+		}
 	}
 }
