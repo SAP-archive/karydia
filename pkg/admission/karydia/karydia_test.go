0197293019913bb17e36d0fa3673f340457ab007
Marc Rahn <marc.rahn@sap.com>
2019-03-26 13:34:16 +0100

    Test different service account and volume combinations

--- pkg/admission/karydia/karydia_test.go
+++ pkg/admission/karydia/karydia_test.go
@@ -20,6 +20,67 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
+// kubectl annotate ns default karydia.gardener.cloud/automountServiceAccountToken=remove-default
+func TestMutatePodWithRemoveDefaultAnnotation(t *testing.T) {
+	pod := corev1.Pod{}
+	var patches []string
+	var validationErrors []string
+
+	pod.Spec.ServiceAccountName = "default"
+	pod.Spec.Volumes = append([]corev1.Volume{}, corev1.Volume{Name: "default-token-abcd", VolumeSource: corev1.VolumeSource{}})
+	mounts := append([]corev1.VolumeMount{}, corev1.VolumeMount{Name: "default-token-abcd"})
+	pod.Spec.Containers = append([]corev1.Container{}, corev1.Container{Name: "first-container", VolumeMounts: mounts})
+
+	patches, validationErrors = secureAutomountServiceAccountToken(pod, "remove-default", patches, validationErrors)
+	if len(patches) != 4 {
+		t.Errorf("expected 4 patches but got: %+v", patches)
+	}
+	if len(validationErrors) != 0 {
+		t.Errorf("expected 0 validationErrors but got: %+v", validationErrors)
+	}
+}
+
+func TestMutatePodWithRemoveDefaultAnnotationNonDefaultServiceAccount(t *testing.T) {
+	pod := corev1.Pod{}
+	var patches []string
+	var validationErrors []string
+
+	pod.Spec.ServiceAccountName = "test"
+	pod.Spec.Volumes = append([]corev1.Volume{}, corev1.Volume{Name: "test-token-abcd", VolumeSource: corev1.VolumeSource{}})
+	mounts := append([]corev1.VolumeMount{}, corev1.VolumeMount{Name: "test-token-abcd"})
+	pod.Spec.Containers = append([]corev1.Container{}, corev1.Container{Name: "first-container", VolumeMounts: mounts})
+
+	patches, validationErrors = secureAutomountServiceAccountToken(pod, "remove-default", patches, validationErrors)
+	if len(patches) != 0 {
+		t.Errorf("expected 0 patches but got: %+v", patches)
+	}
+	if len(validationErrors) != 0 {
+		t.Errorf("expected 0 validationErrors but got: %+v", validationErrors)
+	}
+}
+
+func TestMutatePodWithRemoveDefaultAnnotationMultipleVolumes(t *testing.T) {
+	pod := corev1.Pod{}
+	var patches []string
+	var validationErrors []string
+
+	pod.Spec.ServiceAccountName = "default"
+	pod.Spec.Volumes = append([]corev1.Volume{}, corev1.Volume{Name: "default-token-abcd", VolumeSource: corev1.VolumeSource{}})
+	pod.Spec.Volumes = append(pod.Spec.Volumes, corev1.Volume{Name: "test-token-abcd", VolumeSource: corev1.VolumeSource{}})
+	mounts := append([]corev1.VolumeMount{}, corev1.VolumeMount{Name: "default-token-abcd"})
+	mounts = append(mounts, corev1.VolumeMount{Name: "test-token-abcd"})
+	pod.Spec.Containers = append([]corev1.Container{}, corev1.Container{Name: "first-container", VolumeMounts: mounts})
+
+	patches, validationErrors = secureAutomountServiceAccountToken(pod, "remove-default", patches, validationErrors)
+	if len(patches) != 4 {
+		t.Errorf("expected 4 patches but got: %+v", patches)
+	}
+	if len(validationErrors) != 0 {
+		t.Errorf("expected 0 validationErrors but got: %+v", validationErrors)
+	}
+}
+
+//kubectl annotate ns default karydia.gardener.cloud/automountServiceAccountToken=forbidden
 func TestValidatePodWithForbiddentAnnotation(t *testing.T) {
 	pod := corev1.Pod{}
 	var patches []string
@@ -35,25 +96,8 @@ func TestValidatePodWithForbiddentAnnotation(t *testing.T) {
 		t.Errorf("expected 1 validationErrors but got: %+v", validationErrors)
 	}
 }
-func TestMutatePodWithRemoveDefaultAnnotation(t *testing.T) {
-	pod := corev1.Pod{}
-	var patches []string
-	var validationErrors []string
 
-	pod.Spec.ServiceAccountName = "default"
-	pod.Spec.Volumes = append([]corev1.Volume{}, corev1.Volume{Name: "default-token-abcd", VolumeSource: corev1.VolumeSource{}})
-	mounts := append([]corev1.VolumeMount{}, corev1.VolumeMount{Name: "default-token-1234"})
-	pod.Spec.Containers = append([]corev1.Container{}, corev1.Container{Name: "first-container", VolumeMounts: mounts})
-
-	patches, validationErrors = secureAutomountServiceAccountToken(pod, "remove-default", patches, validationErrors)
-	if len(patches) != 4 {
-		t.Errorf("expected 4 patches but got: %+v", patches)
-	}
-	if len(validationErrors) != 0 {
-		t.Errorf("expected 0 validationErrors but got: %+v", validationErrors)
-	}
-}
-
+//kubectl annotate ns default karydia.gardener.cloud/automountServiceAccountToken=non-default
 func TestValidatePodWithNonDefaultAnnotation(t *testing.T) {
 	pod := corev1.Pod{}
 	var patches []string
@@ -66,5 +110,20 @@ func TestValidatePodWithNonDefaultAnnotation(t *testing.T) {
 	}
 	if len(validationErrors) != 1 {
 		t.Errorf("expected 1 validationErrors but got: %+v", validationErrors)
+	}
+}
+
+func TestValidatePodWithNonDefaultAnnotationNonDefaultServiceAccount(t *testing.T) {
+	pod := corev1.Pod{}
+	var patches []string
+	var validationErrors []string
+
+	pod.Spec.ServiceAccountName = "test"
+	patches, validationErrors = secureAutomountServiceAccountToken(pod, "non-default", patches, validationErrors)
+	if len(patches) != 0 {
+		t.Errorf("expected 0 patches but got: %+v", patches)
+	}
+	if len(validationErrors) != 0 {
+		t.Errorf("expected 0 validationErrors but got: %+v", validationErrors)
 	}
 }
