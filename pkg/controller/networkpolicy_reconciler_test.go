9571310a470e40a83485b91e5d518b8bc83dcec8
Marc Rahn <marc.rahn@sap.com>
2019-04-15 12:59:38 +0200

    Implement reconciler functionality

--- pkg/controller/networkpolicy_reconciler_test.go
+++ pkg/controller/networkpolicy_reconciler_test.go
@@ -17,20 +17,18 @@ limitations under the License.
 package controller
 
 import (
-	"reflect"
+	"bytes"
 	"testing"
 	"time"
 
 	networkingv1 "k8s.io/api/networking/v1"
 
+	coreV1 "k8s.io/api/core/v1"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/util/diff"
 	kubeinformers "k8s.io/client-go/informers"
 	k8sfake "k8s.io/client-go/kubernetes/fake"
-	core "k8s.io/client-go/testing"
 	"k8s.io/client-go/tools/cache"
-	"k8s.io/client-go/tools/record"
 )
 
 var (
@@ -42,15 +40,17 @@ type fixture struct {
 	t *testing.T
 
 	kubeclient *k8sfake.Clientset
+
 	// Objects to put in the store.
 	networkPolicy []*networkingv1.NetworkPolicy
-	// Actions expected to happen on the client.
-	kubeactions []core.Action
-	actions     []core.Action
+	namespace     []*coreV1.Namespace
+
 	// Objects from here preloaded into NewSimpleFake.
 	kubeobjects []runtime.Object
 
 	defaultNetworkPolicy networkingv1.NetworkPolicy
+
+	namespaceExclude []string
 }
 
 func newFixture(t *testing.T) *fixture {
@@ -62,141 +62,57 @@ func newFixture(t *testing.T) *fixture {
 	f.defaultNetworkPolicy.Spec = networkingv1.NetworkPolicySpec{
 		PolicyTypes: []networkingv1.PolicyType{},
 	}
-
+	f.namespaceExclude = []string{"kube-system", "unittestexclude"}
 	return f
 }
 
-func (f *fixture) newController() (*NetworkpolicyReconciler, kubeinformers.SharedInformerFactory) {
+func (f *fixture) newReconciler() (*NetworkpolicyReconciler, kubeinformers.SharedInformerFactory) {
 
 	f.kubeclient = k8sfake.NewSimpleClientset(f.kubeobjects...)
 
 	k8sI := kubeinformers.NewSharedInformerFactory(f.kubeclient, noResyncPeriodFunc())
 
-	c := NewNetworkpolicyReconciler(f.kubeclient, k8sI.Networking().V1().NetworkPolicies(), &f.defaultNetworkPolicy)
+	reconciler := NewNetworkpolicyReconciler(f.kubeclient, k8sI.Networking().V1().NetworkPolicies(), k8sI.Core().V1().Namespaces(), &f.defaultNetworkPolicy, f.namespaceExclude)
 
-	c.networkPoliciesSynced = alwaysReady
-	c.recorder = &record.FakeRecorder{}
+	reconciler.networkPoliciesSynced = alwaysReady
+	reconciler.namespacesSynced = alwaysReady
 
 	for _, d := range f.networkPolicy {
 		k8sI.Networking().V1().NetworkPolicies().Informer().GetIndexer().Add(d)
 	}
 
-	return c, k8sI
-}
-
-func (f *fixture) run(fooName string) {
-	f.runController(fooName, true, false)
-}
-
-func (f *fixture) runExpectError(fooName string) {
-	f.runController(fooName, true, true)
-}
-
-func (f *fixture) runController(fooName string, startInformers bool, expectError bool) {
-	c, k8sI := f.newController()
-	if startInformers {
-		stopCh := make(chan struct{})
-		defer close(stopCh)
-		k8sI.Start(stopCh)
+	for _, d := range f.namespace {
+		k8sI.Core().V1().Namespaces().Informer().GetIndexer().Add(d)
 	}
 
-	err := c.syncHandler(fooName)
-	if !expectError && err != nil {
+	return reconciler, k8sI
+}
+
+func (f *fixture) runReconcile(networkPolicyName string) {
+
+	reconciler, k8sI := f.newReconciler()
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+	k8sI.Start(stopCh)
+
+	err := reconciler.syncNetworkPolicyHandler(networkPolicyName)
+	if err != nil {
+		f.t.Errorf("error syncing networkpolicy: %v", err)
+	}
+}
+
+func (f *fixture) runNamespaceAdd(namespace string) {
+
+	reconciler, k8sI := f.newReconciler()
+
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+	k8sI.Start(stopCh)
+
+	err := reconciler.syncNamespaceHandler(namespace)
+	if err != nil {
 		f.t.Errorf("error syncing foo: %v", err)
-	} else if expectError && err == nil {
-		f.t.Error("expected error syncing foo, got nil")
 	}
-
-	actions := filterInformerActions(f.kubeclient.Actions())
-	for i, action := range actions {
-		if len(f.actions) < i+1 {
-			f.t.Errorf("%d unexpected actions: %+v", len(actions)-len(f.actions), actions[i:])
-			break
-		}
-
-		expectedAction := f.actions[i]
-		checkAction(expectedAction, action, f.t)
-	}
-
-	if len(f.actions) > len(actions) {
-		f.t.Errorf("%d additional expected actions:%+v", len(f.actions)-len(actions), f.actions[len(actions):])
-	}
-
-	k8sActions := filterInformerActions(f.kubeclient.Actions())
-	for i, action := range k8sActions {
-		if len(f.kubeactions) < i+1 {
-			f.t.Errorf("%d unexpected actions: %+v", len(k8sActions)-len(f.kubeactions), k8sActions[i:])
-			break
-		}
-
-		expectedAction := f.kubeactions[i]
-		checkAction(expectedAction, action, f.t)
-	}
-
-	if len(f.kubeactions) > len(k8sActions) {
-		f.t.Errorf("%d additional expected actions:%+v", len(f.kubeactions)-len(k8sActions), f.kubeactions[len(k8sActions):])
-	}
-}
-
-// checkAction verifies that expected and actual actions are equal and both have
-// same attached resources
-func checkAction(expected, actual core.Action, t *testing.T) {
-	if !(expected.Matches(actual.GetVerb(), actual.GetResource().Resource) && actual.GetSubresource() == expected.GetSubresource()) {
-		t.Errorf("Expected\n\t%#v\ngot\n\t%#v", expected, actual)
-		return
-	}
-
-	if reflect.TypeOf(actual) != reflect.TypeOf(expected) {
-		t.Errorf("Action has wrong type. Expected: %t. Got: %t", expected, actual)
-		return
-	}
-
-	switch a := actual.(type) {
-	case core.CreateAction:
-		e, _ := expected.(core.CreateAction)
-		expObject := e.GetObject()
-		object := a.GetObject()
-
-		if !reflect.DeepEqual(expObject, object) {
-			t.Errorf("Action %s %s has wrong object\nDiff:\n %s",
-				a.GetVerb(), a.GetResource().Resource, diff.ObjectGoPrintDiff(expObject, object))
-		}
-	case core.UpdateAction:
-		e, _ := expected.(core.UpdateAction)
-		expObject := e.GetObject()
-		object := a.GetObject()
-
-		if !reflect.DeepEqual(expObject, object) {
-			t.Errorf("Action %s %s has wrong object\nDiff:\n %s",
-				a.GetVerb(), a.GetResource().Resource, diff.ObjectGoPrintDiff(expObject, object))
-		}
-	case core.PatchAction:
-		e, _ := expected.(core.PatchAction)
-		expPatch := e.GetPatch()
-		patch := a.GetPatch()
-
-		if !reflect.DeepEqual(expPatch, patch) {
-			t.Errorf("Action %s %s has wrong patch\nDiff:\n %s",
-				a.GetVerb(), a.GetResource().Resource, diff.ObjectGoPrintDiff(expPatch, patch))
-		}
-	}
-}
-
-// filterInformerActions filters list and watch actions for testing resources.
-// Since list and watch don't change resource state we can filter it to lower
-// nose level in our tests.
-func filterInformerActions(actions []core.Action) []core.Action {
-	ret := []core.Action{}
-	for _, action := range actions {
-		if len(action.GetNamespace()) == 0 &&
-			(action.Matches("list", "networkpolicies") ||
-				action.Matches("watch", "networkpolicies")) {
-			continue
-		}
-		ret = append(ret, action)
-	}
-
-	return ret
 }
 
 func getKey(networkpolicy *networkingv1.NetworkPolicy, t *testing.T) string {
@@ -208,7 +124,13 @@ func getKey(networkpolicy *networkingv1.NetworkPolicy, t *testing.T) string {
 	return key
 }
 
-func TestCreateNetworkPolicy(t *testing.T) {
+func networkPoliciesAreEqual(defaultNetworkPolicy, networkPolicy *networkingv1.NetworkPolicy) bool {
+	actualSpec, _ := networkPolicy.Spec.Marshal()
+	desiredSpec, _ := defaultNetworkPolicy.Spec.Marshal()
+	return bytes.Equal(actualSpec, desiredSpec)
+}
+
+func TestReconcileNetworkPolicyUpate(t *testing.T) {
 	f := newFixture(t)
 	newNetworkPolicy := &networkingv1.NetworkPolicy{}
 	newNetworkPolicy.Name = "karydia-default-network-policy"
@@ -219,12 +141,60 @@ func TestCreateNetworkPolicy(t *testing.T) {
 	f.networkPolicy = append(f.networkPolicy, newNetworkPolicy)
 	f.kubeobjects = append(f.kubeobjects, newNetworkPolicy)
 
-	f.run(getKey(newNetworkPolicy, t))
+	f.runReconcile(getKey(newNetworkPolicy, t))
 
-	actualPolicy, err := f.kubeclient.NetworkingV1().NetworkPolicies(newNetworkPolicy.Namespace).Get(newNetworkPolicy.Name, meta_v1.GetOptions{})
+	reconciledPolicy, err := f.kubeclient.NetworkingV1().NetworkPolicies(newNetworkPolicy.Namespace).Get(newNetworkPolicy.Name, meta_v1.GetOptions{})
 	if err != nil {
 		t.Errorf("No error expected")
-	} else if len(actualPolicy.Spec.PolicyTypes) != 0 {
-		t.Errorf("No reconcile")
+	} else if !networkPoliciesAreEqual(&f.defaultNetworkPolicy, reconciledPolicy) {
+		t.Errorf("No reconcilation happened")
+	}
+}
+
+func TestReconcileNetworkPolicyDelete(t *testing.T) {
+	f := newFixture(t)
+	newNetworkPolicy := &networkingv1.NetworkPolicy{}
+	newNetworkPolicy.Name = "karydia-default-network-policy"
+	newNetworkPolicy.Namespace = "default"
+
+	f.runReconcile(getKey(newNetworkPolicy, t))
+
+	reconciledPolicy, err := f.kubeclient.NetworkingV1().NetworkPolicies(newNetworkPolicy.Namespace).Get(newNetworkPolicy.Name, meta_v1.GetOptions{})
+	if err != nil {
+		t.Errorf("No error expected")
+	} else if !networkPoliciesAreEqual(&f.defaultNetworkPolicy, reconciledPolicy) {
+		t.Errorf("No reconcilation happened")
+	}
+}
+
+func TestReconcileNetworkPolicyCreateNamespace(t *testing.T) {
+	f := newFixture(t)
+	newNamespace := &coreV1.Namespace{}
+	newNamespace.Name = "unittest"
+
+	f.namespace = append(f.namespace, newNamespace)
+	f.kubeobjects = append(f.kubeobjects, newNamespace)
+
+	f.runNamespaceAdd(newNamespace.Name)
+	reconciledPolicy, err := f.kubeclient.NetworkingV1().NetworkPolicies(newNamespace.Name).Get(f.defaultNetworkPolicy.Name, meta_v1.GetOptions{})
+	if err != nil {
+		t.Errorf("No error expected")
+	} else if !networkPoliciesAreEqual(&f.defaultNetworkPolicy, reconciledPolicy) {
+		t.Errorf("No reconcilation happened")
+	}
+}
+
+func TestReconcileNetworkPolicyCreateExcludedNamespace(t *testing.T) {
+	f := newFixture(t)
+	newNamespace := &coreV1.Namespace{}
+	newNamespace.Name = "unittestexclude"
+
+	f.namespace = append(f.namespace, newNamespace)
+	f.kubeobjects = append(f.kubeobjects, newNamespace)
+
+	f.runNamespaceAdd(newNamespace.Name)
+	reconciledPolicy, _ := f.kubeclient.NetworkingV1().NetworkPolicies(newNamespace.Name).Get(f.defaultNetworkPolicy.Name, meta_v1.GetOptions{})
+	if reconciledPolicy != nil {
+		t.Errorf("Reconcilation happened - default network policy created for excluded namespace")
 	}
 }
