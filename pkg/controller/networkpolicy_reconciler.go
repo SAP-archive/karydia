cfd9faa2d9553023689065167603a8588c8b49ae
Marc Rahn <marc.rahn@sap.com>
2019-04-15 12:59:38 +0200

    Implement reconciler functionality

--- pkg/controller/networkpolicy_reconciler.go
+++ pkg/controller/networkpolicy_reconciler.go
@@ -17,83 +17,64 @@ limitations under the License.
 package controller
 
 import (
+	"bytes"
 	"fmt"
 	"time"
 
 	networkingv1 "k8s.io/api/networking/v1"
+	v1 "k8s.io/api/networking/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
+	namespaceInformer "k8s.io/client-go/informers/core/v1"
 	networkpolicyInformer "k8s.io/client-go/informers/networking/v1"
 	"k8s.io/client-go/kubernetes"
+	kubelistersv1 "k8s.io/client-go/listers/core/v1"
 	kubelistersNetworkingv1 "k8s.io/client-go/listers/networking/v1"
 	"k8s.io/client-go/tools/cache"
-	"k8s.io/client-go/tools/record"
 	"k8s.io/client-go/util/workqueue"
 	"k8s.io/klog"
 )
 
 const controllerAgentName = "networkpolicy_reconciler"
 
-const (
-	// SuccessSynced is used as part of the Event 'reason' when a Foo is synced
-	SuccessSynced = "Synced"
-	// ErrResourceExists is used as part of the Event 'reason' when a Foo fails
-	// to sync due to a Deployment of the same name already existing.
-	ErrResourceExists = "ErrResourceExists"
-
-	// MessageResourceExists is the message used for Events when a resource
-	// fails to sync due to a Deployment already existing
-	MessageResourceExists = "Resource %q already exists and is not managed by Foo"
-	// MessageResourceSynced is the message used for an Event fired when a Foo
-	// is synced successfully
-	MessageResourceSynced = "Karydia Networkpolicy synced successfully"
-)
-
-// Controller is the controller implementation for Foo resources
 type NetworkpolicyReconciler struct {
-	defaultNetworkPolicy *networkingv1.NetworkPolicy
+	defaultNetworkPolicy         *networkingv1.NetworkPolicy
+	defaultNetworkPolicyExcludes []string
 
-	// kubeclientset is a standard kubernetes clientset
-	kubeclientset         kubernetes.Interface
-	networkPolicyLister   kubelistersNetworkingv1.NetworkPolicyLister
-	networkPoliciesSynced cache.InformerSynced
-
-	// workqueue is a rate limited work queue. This is used to queue work to be
-	// processed instead of performing it as soon as a change happens. This
-	// means we can ensure we only process a fixed amount of resources at a
-	// time, and makes it easy to ensure we are never processing the same item
-	// simultaneously in two different workers.
-	workqueue workqueue.RateLimitingInterface
-	// recorder is an event recorder for recording Event resources to the
-	// Kubernetes API.
-	recorder record.EventRecorder
+	kubeclientset          kubernetes.Interface
+	networkPolicyLister    kubelistersNetworkingv1.NetworkPolicyLister
+	networkPoliciesSynced  cache.InformerSynced
+	namespacesLister       kubelistersv1.NamespaceLister
+	namespacesSynced       cache.InformerSynced
+	networkPolicyworkqueue workqueue.RateLimitingInterface
+	namespaceWorkqueue     workqueue.RateLimitingInterface
 }
 
-// NewController returns a new sample controller
 func NewNetworkpolicyReconciler(
 	kubeclientset kubernetes.Interface,
-	networkpolicyInformer networkpolicyInformer.NetworkPolicyInformer,
-	defaultNetworkPolicy *networkingv1.NetworkPolicy) *NetworkpolicyReconciler {
+	networkpolicyInformer networkpolicyInformer.NetworkPolicyInformer, namespaceInformer namespaceInformer.NamespaceInformer,
+	defaultNetworkPolicy *networkingv1.NetworkPolicy, defaultNetworkPolicyExcludes []string) *NetworkpolicyReconciler {
 
 	reconciler := &NetworkpolicyReconciler{
 
-		kubeclientset:         kubeclientset,
-		networkPolicyLister:   networkpolicyInformer.Lister(),
-		networkPoliciesSynced: networkpolicyInformer.Informer().HasSynced,
-		workqueue:             workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "Networkpolicies"),
-		defaultNetworkPolicy:  defaultNetworkPolicy,
+		kubeclientset:                kubeclientset,
+		networkPolicyLister:          networkpolicyInformer.Lister(),
+		networkPoliciesSynced:        networkpolicyInformer.Informer().HasSynced,
+		namespacesLister:             namespaceInformer.Lister(),
+		namespacesSynced:             namespaceInformer.Informer().HasSynced,
+		namespaceWorkqueue:           workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "Namespaces"),
+		networkPolicyworkqueue:       workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "Networkpolicies"),
+		defaultNetworkPolicy:         defaultNetworkPolicy,
+		defaultNetworkPolicyExcludes: defaultNetworkPolicyExcludes,
 	}
 
 	klog.Info("Setting up event handlers")
-	// Set up an event handler for when Foo resources change
 	networkpolicyInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
 		UpdateFunc: func(old, new interface{}) {
 			newNetworkPolicy := new.(*networkingv1.NetworkPolicy)
 			oldNetworkPolicy := old.(*networkingv1.NetworkPolicy)
-			if newNetworkPolicy.ResourceVersion == oldNetworkPolicy.ResourceVersion || oldNetworkPolicy.GetName() != reconciler.defaultNetworkPolicy.GetName() {
-				// Periodic resync will send update events for all known Deployments.
-				// Two different versions of the same Deployment will always have different RVs.
+			if newNetworkPolicy.ResourceVersion == oldNetworkPolicy.ResourceVersion {
 				return
 			}
 			reconciler.enqueueNetworkPolicy(new)
@@ -101,30 +82,29 @@ func NewNetworkpolicyReconciler(
 		DeleteFunc: reconciler.enqueueNetworkPolicy,
 	})
 
+	namespaceInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: reconciler.enqueueNamespace,
+	})
+
 	return reconciler
 }
 
-// Run will set up the event handlers for types we are interested in, as well
-// as syncing informer caches and starting workers. It will block until stopCh
-// is closed, at which point it will shutdown the workqueue and wait for
-// workers to finish processing their current work items.
 func (reconciler *NetworkpolicyReconciler) Run(threadiness int, stopCh <-chan struct{}) error {
 	defer utilruntime.HandleCrash()
-	defer reconciler.workqueue.ShutDown()
+	defer reconciler.networkPolicyworkqueue.ShutDown()
+	defer reconciler.namespaceWorkqueue.ShutDown()
 
-	// Start the informer factories to begin populating the informer caches
-	klog.Info("Starting Foo controller")
-
-	// Wait for the caches to be synced before starting workers
+	klog.Info("Starting karydia network policy reconciler")
 	klog.Info("Waiting for informer caches to sync")
-	if ok := cache.WaitForCacheSync(stopCh, reconciler.networkPoliciesSynced); !ok {
+	if ok := cache.WaitForCacheSync(stopCh, reconciler.networkPoliciesSynced, reconciler.namespacesSynced); !ok {
 		return fmt.Errorf("failed to wait for caches to sync")
 	}
 
 	klog.Info("Starting workers")
-	// Launch two workers to process Foo resources
+
 	for i := 0; i < threadiness; i++ {
-		go wait.Until(reconciler.runWorker, time.Second, stopCh)
+		go wait.Until(reconciler.runNetworkPolicyWorker, time.Second, stopCh)
+		go wait.Until(reconciler.runNamespaceWorker, time.Second, stopCh)
 	}
 
 	klog.Info("Started workers")
@@ -134,57 +114,40 @@ func (reconciler *NetworkpolicyReconciler) Run(threadiness int, stopCh <-chan st
 	return nil
 }
 
-// runWorker is a long-running function that will continually call the
-// processNextWorkItem function in order to read and process a message on the
-// workqueue.
-func (reconciler *NetworkpolicyReconciler) runWorker() {
-	for reconciler.processNextWorkItem() {
+func (reconciler *NetworkpolicyReconciler) runNetworkPolicyWorker() {
+	for reconciler.processNextNetworkPolicyWorkItem() {
+	}
+}
+func (reconciler *NetworkpolicyReconciler) runNamespaceWorker() {
+	for reconciler.processNextNamespaceWorkItem() {
 	}
 }
 
-// processNextWorkItem will read a single work item off the workqueue and
-// attempt to process it, by calling the syncHandler.
-func (reconciler *NetworkpolicyReconciler) processNextWorkItem() bool {
-	obj, shutdown := reconciler.workqueue.Get()
+func (reconciler *NetworkpolicyReconciler) processNextNetworkPolicyWorkItem() bool {
+	obj, shutdown := reconciler.networkPolicyworkqueue.Get()
 
 	if shutdown {
 		return false
 	}
 
-	// We wrap this block in a func so we can defer c.workqueue.Done.
 	err := func(obj interface{}) error {
-		// We call Done here so the workqueue knows we have finished
-		// processing this item. We also must remember to call Forget if we
-		// do not want this work item being re-queued. For example, we do
-		// not call Forget if a transient error occurs, instead the item is
-		// put back on the workqueue and attempted again after a back-off
-		// period.
-		defer reconciler.workqueue.Done(obj)
+		defer reconciler.networkPolicyworkqueue.Done(obj)
 		var key string
 		var ok bool
-		// We expect strings to come off the workqueue. These are of the
-		// form namespace/name. We do this as the delayed nature of the
-		// workqueue means the items in the informer cache may actually be
-		// more up to date that when the item was initially put onto the
-		// workqueue.
+
 		if key, ok = obj.(string); !ok {
-			// As the item in the workqueue is actually invalid, we call
-			// Forget here else we'd go into a loop of attempting to
-			// process a work item that is invalid.
-			reconciler.workqueue.Forget(obj)
+			reconciler.networkPolicyworkqueue.Forget(obj)
 			utilruntime.HandleError(fmt.Errorf("expected string in workqueue but got %#v", obj))
 			return nil
 		}
-		// Run the syncHandler, passing it the namespace/name string of the
-		// Foo resource to be synced.
-		if err := reconciler.syncHandler(key); err != nil {
-			// Put the item back on the workqueue to handle any transient errors.
-			reconciler.workqueue.AddRateLimited(key)
+
+		if err := reconciler.syncNetworkPolicyHandler(key); err != nil {
+
+			reconciler.networkPolicyworkqueue.AddRateLimited(key)
 			return fmt.Errorf("error syncing '%s': %s, requeuing", key, err.Error())
 		}
-		// Finally, if no error occurs we Forget this item so it does not
-		// get queued again until another change happens.
-		reconciler.workqueue.Forget(obj)
+
+		reconciler.networkPolicyworkqueue.Forget(obj)
 		klog.Infof("Successfully synced '%s'", key)
 		return nil
 	}(obj)
@@ -197,39 +160,114 @@ func (reconciler *NetworkpolicyReconciler) processNextWorkItem() bool {
 	return true
 }
 
-// syncHandler compares the actual state with the desired, and attempts to
-// converge the two. It then updates the Status block of the Foo resource
-// with the current status of the resource.
-func (reconciler *NetworkpolicyReconciler) syncHandler(key string) error {
-	// Convert the namespace/name string into a distinct namespace and name
+func (reconciler *NetworkpolicyReconciler) processNextNamespaceWorkItem() bool {
+	obj, shutdown := reconciler.namespaceWorkqueue.Get()
+
+	if shutdown {
+		return false
+	}
+
+	err := func(obj interface{}) error {
+		defer reconciler.namespaceWorkqueue.Done(obj)
+		var key string
+		var ok bool
+
+		if key, ok = obj.(string); !ok {
+
+			reconciler.namespaceWorkqueue.Forget(obj)
+			utilruntime.HandleError(fmt.Errorf("expected string in workqueue but got %#v", obj))
+			return nil
+		}
+
+		if err := reconciler.syncNamespaceHandler(key); err != nil {
+			reconciler.namespaceWorkqueue.AddRateLimited(key)
+			return fmt.Errorf("error syncing '%s': %s, requeuing", key, err.Error())
+		}
+
+		reconciler.namespaceWorkqueue.Forget(obj)
+		klog.Infof("Successfully synced '%s'", key)
+		return nil
+	}(obj)
+
+	if err != nil {
+		utilruntime.HandleError(err)
+		return true
+	}
+
+	return true
+}
+
+func (reconciler *NetworkpolicyReconciler) syncNetworkPolicyHandler(key string) error {
 	namespace, name, err := cache.SplitMetaNamespaceKey(key)
 	if err != nil {
 		utilruntime.HandleError(fmt.Errorf("invalid resource key: %s", key))
 		return nil
 	}
+	if reconciler.defaultNetworkPolicy == nil {
+		klog.Warning("No default network policy set")
+		reconciler.namespaceWorkqueue.Forget(key)
+		return nil
+	}
 
-	// Get the Foo resource with this namespace/name
-	networkPolicy, err := reconciler.networkPolicyLister.NetworkPolicies(namespace).Get(name)
+	if name == reconciler.defaultNetworkPolicy.GetName() {
 
-	if err != nil {
-		// The Foo resource may no longer exist, in which case we stop
-		// processing.
-		if errors.IsNotFound(err) {
-			utilruntime.HandleError(fmt.Errorf("networkpolicy '%s' in work queue no longer exists", key))
-			// CHeck if name == defaultNetworkPolicyName, if yes recreate it
-			return nil
+		networkPolicy, err := reconciler.networkPolicyLister.NetworkPolicies(namespace).Get(name)
+		if err != nil {
+			if errors.IsNotFound(err) {
+				utilruntime.HandleError(fmt.Errorf("networkpolicy '%s' in work queue no longer exists", key))
+				if err := reconciler.createDefaultNetworkPolicy(namespace); err != nil {
+					klog.Errorf("failed to recreate default network policy: %v", err)
+					return fmt.Errorf("error syncing %q: %v", namespace, err)
+				}
+				return nil
+			}
+		} else {
+			klog.Infof("Found networkpolicy %s/%s", namespace, networkPolicy.Name)
+			if reconciler.reconcileIsNeeded(networkPolicy) {
+				if err := reconciler.updateDefaultNetworkPolicy(namespace); err != nil {
+					klog.Errorf("failed to update default network policy: %v", err)
+					return fmt.Errorf("error syncing %q: %v", namespace, err)
+				}
+			}
 		}
-
-		klog.Infof("Found networkpolicy %s", networkPolicy.Name)
-
-		return err
 	}
 	return nil
 }
+func (reconciler *NetworkpolicyReconciler) syncNamespaceHandler(key string) error {
 
-// enqueueFoo takes a Foo resource and converts it into a namespace/name
-// string which is then put onto the work queue. This method should *not* be
-// passed resources of any type other than Foo.
+	_, err := reconciler.namespacesLister.Get(key)
+
+	if err != nil {
+		if errors.IsNotFound(err) {
+			utilruntime.HandleError(fmt.Errorf("namespace '%s' in work queue no longer exists", key))
+			return nil
+		}
+	}
+	klog.Infof("Found namespace %s", key)
+	for _, ns := range reconciler.defaultNetworkPolicyExcludes {
+		if key == ns {
+			klog.Infof("Not creating default network policy in %q - namespace excluded", key)
+			reconciler.namespaceWorkqueue.Forget(key)
+			return nil
+		}
+	}
+
+	if reconciler.defaultNetworkPolicy == nil {
+		klog.Warning("No default network policy set")
+		reconciler.namespaceWorkqueue.Forget(key)
+		return nil
+	}
+
+	if err := reconciler.createDefaultNetworkPolicy(key); err != nil {
+		klog.Errorf("failed to create default network policy: %v", err)
+		return fmt.Errorf("error syncing %q: %v", key, err)
+	}
+
+	reconciler.namespaceWorkqueue.Forget(key)
+	klog.Infof("Successfully synced %q", key)
+	return nil
+}
+
 func (reconciler *NetworkpolicyReconciler) enqueueNetworkPolicy(obj interface{}) {
 	var key string
 	var err error
@@ -237,42 +275,45 @@ func (reconciler *NetworkpolicyReconciler) enqueueNetworkPolicy(obj interface{})
 		utilruntime.HandleError(err)
 		return
 	}
-	reconciler.workqueue.AddRateLimited(key)
+	reconciler.networkPolicyworkqueue.AddRateLimited(key)
+}
+func (reconciler *NetworkpolicyReconciler) enqueueNamespace(obj interface{}) {
+	var key string
+	var err error
+	if key, err = cache.MetaNamespaceKeyFunc(obj); err != nil {
+		utilruntime.HandleError(err)
+		return
+	}
+	reconciler.namespaceWorkqueue.AddRateLimited(key)
 }
 
-// func (reconciler *NetworkpolicyReconciler) defaultNetworkPolicyExists(namespace string) bool {
-// 	if _, err := reconciler.kubeClientset.NetworkingV1().NetworkPolicies(namespace).Get(reconciler.defaultNetworkPolicy.GetName(), meta_v1.GetOptions{}); err == nil {
-// 		return true
-// 	} else {
-// 		return false
-// 	}
+func (reconciler *NetworkpolicyReconciler) reconcileIsNeeded(actualPolicy *v1.NetworkPolicy) bool {
+	desiredPolicy := reconciler.defaultNetworkPolicy.DeepCopy()
+	actualSpec, _ := actualPolicy.Spec.Marshal()
+	desiredSpec, _ := desiredPolicy.Spec.Marshal()
+	if bytes.Equal(actualSpec, desiredSpec) {
+		return false
+	}
+	return true
 
-// }
+}
 
-// func (reconciler *NetworkpolicyReconciler) updateDefaultNetworkPolicy(namespace string) error {
-// 	desiredPolicy := reconciler.defaultNetworkPolicy.DeepCopy()
-// 	desiredPolicy.ObjectMeta.Namespace = namespace
+func (reconciler *NetworkpolicyReconciler) updateDefaultNetworkPolicy(namespace string) error {
+	desiredPolicy := reconciler.defaultNetworkPolicy.DeepCopy()
+	desiredPolicy.Namespace = namespace
+	if _, err := reconciler.kubeclientset.NetworkingV1().NetworkPolicies(namespace).Update(desiredPolicy); err != nil {
+		return err
+	}
+	return nil
+}
 
-// 	if actualPolicy, err := reconciler.kubeClientset.NetworkingV1().NetworkPolicies(namespace).Get(desiredPolicy.GetName(), meta_v1.GetOptions{}); err == nil {
-// 		actualSpec, _ := actualPolicy.Spec.Marshal()
-// 		desiredSpec, _ := desiredPolicy.Spec.Marshal()
-// 		if bytes.Equal(actualSpec, desiredSpec) {
-// 			return nil
-// 		} else if _, err := reconciler.kubeClientset.NetworkingV1().NetworkPolicies(namespace).Update(desiredPolicy); err != nil {
-// 			return err
-// 		}
-// 	}
-// 	return nil
+func (reconciler *NetworkpolicyReconciler) createDefaultNetworkPolicy(namespace string) error {
+	desiredPolicy := reconciler.defaultNetworkPolicy.DeepCopy()
+	desiredPolicy.ObjectMeta.Namespace = namespace
 
-// }
+	if _, err := reconciler.kubeclientset.NetworkingV1().NetworkPolicies(namespace).Create(desiredPolicy); err != nil {
+		return err
+	}
+	return nil
 
-// func (reconciler *NetworkpolicyReconciler) createDefaultNetworkPolicy(namespace string) error {
-// 	desiredPolicy := reconciler.defaultNetworkPolicy.DeepCopy()
-// 	desiredPolicy.ObjectMeta.Namespace = namespace
-
-// 	if _, err := reconciler.kubeClientset.NetworkingV1().NetworkPolicies(namespace).Create(desiredPolicy); err != nil {
-// 		return err
-// 	}
-// 	return nil
-
-// }
+}
