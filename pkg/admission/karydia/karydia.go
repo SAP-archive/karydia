SAP-archive/karydia

commit 2735a29c16be9f09f6e190ee212330b7b8d1d799

Distinguish between mutating and validating service token pod webhook. (#84)
Option karydia.gardener.cloud/automountServiceAccountToken=remove-default
is only applicable for the mutating webhook. If the mutating hook
is not registered at the api server, but the option remove-default
is set, karydia will return a meaningful error message and will deny
the deployment.

pkg/admission/karydia/karydia.go
@@ -82,7 +82,7 @@ func (k *KarydiaAdmission) AdmitPod(admissionRequest v1beta1.AdmissionRequest, m
 
 	automountServiceAccountToken, annotated := namespace.ObjectMeta.Annotations["karydia.gardener.cloud/automountServiceAccountToken"]
 	if annotated {
-		patches, validationErrors = admitServiceAccountToken(*pod, automountServiceAccountToken, patches, validationErrors)
+		patches, validationErrors = admitServiceAccountToken(*pod, automountServiceAccountToken, mutationAllowed, patches, validationErrors)
 	}
 
 	seccompProfile, annotated := namespace.ObjectMeta.Annotations["karydia.gardener.cloud/seccompProfile"]
@@ -93,7 +93,7 @@ func (k *KarydiaAdmission) AdmitPod(admissionRequest v1beta1.AdmissionRequest, m
 	return admitResponse(patches, validationErrors)
 }
 
-func admitServiceAccountToken(pod corev1.Pod, annotation string, patches []string, validationErrors []string) ([]string, []string) {
+func admitServiceAccountToken(pod corev1.Pod, annotation string, mutationAllowed bool, patches []string, validationErrors []string) ([]string, []string) {
 	if annotation == "forbidden" {
 		// Validating webhook
 		if automountServiceAccountTokenUndefined(&pod) {
@@ -106,21 +106,26 @@ func admitServiceAccountToken(pod corev1.Pod, annotation string, patches []strin
 		}
 	} else if annotation == "remove-default" {
 		// Mutating webhook
-		if automountServiceAccountTokenUndefined(&pod) && pod.Spec.ServiceAccountName == "default" {
-			patches = append(patches, fmt.Sprintf(`{"op": "add", "path": "/spec/automountServiceAccountToken", "value": %s}`, "false"))
-			patches = append(patches, fmt.Sprintf(`{"op": "remove", "path": "/spec/serviceAccountName"}`))
-			for i, v := range pod.Spec.Volumes {
-				if strings.HasPrefix(v.Name, "default-token-") {
-					patches = append(patches, fmt.Sprintf(`{"op": "remove", "path": "/spec/volumes/%d"}`, i))
-				}
-			}
-			for i, c := range pod.Spec.Containers {
-				for j, v := range c.VolumeMounts {
+		if mutationAllowed {
+
+			if automountServiceAccountTokenUndefined(&pod) && pod.Spec.ServiceAccountName == "default" {
+				patches = append(patches, fmt.Sprintf(`{"op": "add", "path": "/spec/automountServiceAccountToken", "value": %s}`, "false"))
+				patches = append(patches, fmt.Sprintf(`{"op": "remove", "path": "/spec/serviceAccountName"}`))
+				for i, v := range pod.Spec.Volumes {
 					if strings.HasPrefix(v.Name, "default-token-") {
-						patches = append(patches, fmt.Sprintf(`{"op": "remove", "path": "/spec/containers/%d/volumeMounts/%d"}`, i, j))
+						patches = append(patches, fmt.Sprintf(`{"op": "remove", "path": "/spec/volumes/%d"}`, i))
+					}
+				}
+				for i, c := range pod.Spec.Containers {
+					for j, v := range c.VolumeMounts {
+						if strings.HasPrefix(v.Name, "default-token-") {
+							patches = append(patches, fmt.Sprintf(`{"op": "remove", "path": "/spec/containers/%d/volumeMounts/%d"}`, i, j))
+						}
 					}
 				}
 			}
+		} else {
+			validationErrors = append(validationErrors, "option 'remove-default' for  karydia.gardener.cloud/automountServiceAccountToken is only available for mutating webhooks")
 		}
 	}
 	return patches, validationErrors

pkg/admission/karydia/service_token_test.go
@@ -25,13 +25,14 @@ func TestMutatePodWithRemoveDefaultAnnotation(t *testing.T) {
 	pod := corev1.Pod{}
 	var patches []string
 	var validationErrors []string
+	mutationAllowed := true
 
 	pod.Spec.ServiceAccountName = "default"
 	pod.Spec.Volumes = append([]corev1.Volume{}, corev1.Volume{Name: "default-token-abcd", VolumeSource: corev1.VolumeSource{}})
 	mounts := append([]corev1.VolumeMount{}, corev1.VolumeMount{Name: "default-token-abcd"})
 	pod.Spec.Containers = append([]corev1.Container{}, corev1.Container{Name: "first-container", VolumeMounts: mounts})
 
-	patches, validationErrors = admitServiceAccountToken(pod, "remove-default", patches, validationErrors)
+	patches, validationErrors = admitServiceAccountToken(pod, "remove-default", mutationAllowed, patches, validationErrors)
 	if len(patches) != 4 {
 		t.Errorf("expected 4 patches but got: %+v", patches)
 	}
@@ -40,17 +41,39 @@ func TestMutatePodWithRemoveDefaultAnnotation(t *testing.T) {
 	}
 }
 
+func TestValidatePodWithRemoveDefaultAnnotation(t *testing.T) {
+	pod := corev1.Pod{}
+	var patches []string
+	var validationErrors []string
+
+	mutationAllowed := false
+
+	pod.Spec.ServiceAccountName = "default"
+	pod.Spec.Volumes = append([]corev1.Volume{}, corev1.Volume{Name: "default-token-abcd", VolumeSource: corev1.VolumeSource{}})
+	mounts := append([]corev1.VolumeMount{}, corev1.VolumeMount{Name: "default-token-abcd"})
+	pod.Spec.Containers = append([]corev1.Container{}, corev1.Container{Name: "first-container", VolumeMounts: mounts})
+
+	patches, validationErrors = admitServiceAccountToken(pod, "remove-default", mutationAllowed, patches, validationErrors)
+	if len(patches) != 0 {
+		t.Errorf("expected 0 patches but got: %+v", patches)
+	}
+	if len(validationErrors) != 1 {
+		t.Errorf("expected 1 validationErrors but got: %+v", validationErrors)
+	}
+}
+
 func TestMutatePodWithRemoveDefaultAnnotationNonDefaultServiceAccount(t *testing.T) {
 	pod := corev1.Pod{}
 	var patches []string
 	var validationErrors []string
+	mutationAllowed := true
 
 	pod.Spec.ServiceAccountName = "test"
 	pod.Spec.Volumes = append([]corev1.Volume{}, corev1.Volume{Name: "test-token-abcd", VolumeSource: corev1.VolumeSource{}})
 	mounts := append([]corev1.VolumeMount{}, corev1.VolumeMount{Name: "test-token-abcd"})
 	pod.Spec.Containers = append([]corev1.Container{}, corev1.Container{Name: "first-container", VolumeMounts: mounts})
 
-	patches, validationErrors = admitServiceAccountToken(pod, "remove-default", patches, validationErrors)
+	patches, validationErrors = admitServiceAccountToken(pod, "remove-default", mutationAllowed, patches, validationErrors)
 	if len(patches) != 0 {
 		t.Errorf("expected 0 patches but got: %+v", patches)
 	}
@@ -63,6 +86,7 @@ func TestMutatePodWithRemoveDefaultAnnotationMultipleVolumes(t *testing.T) {
 	pod := corev1.Pod{}
 	var patches []string
 	var validationErrors []string
+	mutationAllowed := true
 
 	pod.Spec.ServiceAccountName = "default"
 	pod.Spec.Volumes = append([]corev1.Volume{}, corev1.Volume{Name: "default-token-abcd", VolumeSource: corev1.VolumeSource{}})
@@ -71,7 +95,7 @@ func TestMutatePodWithRemoveDefaultAnnotationMultipleVolumes(t *testing.T) {
 	mounts = append(mounts, corev1.VolumeMount{Name: "test-token-abcd"})
 	pod.Spec.Containers = append([]corev1.Container{}, corev1.Container{Name: "first-container", VolumeMounts: mounts})
 
-	patches, validationErrors = admitServiceAccountToken(pod, "remove-default", patches, validationErrors)
+	patches, validationErrors = admitServiceAccountToken(pod, "remove-default", mutationAllowed, patches, validationErrors)
 	if len(patches) != 4 {
 		t.Errorf("expected 4 patches but got: %+v", patches)
 	}
@@ -85,10 +109,11 @@ func TestValidatePodWithForbiddentAnnotation(t *testing.T) {
 	pod := corev1.Pod{}
 	var patches []string
 	var validationErrors []string
+	mutationAllowed := true
 
 	pod.Spec.ServiceAccountName = "default"
 
-	patches, validationErrors = admitServiceAccountToken(pod, "forbidden", patches, validationErrors)
+	patches, validationErrors = admitServiceAccountToken(pod, "forbidden", mutationAllowed, patches, validationErrors)
 	if len(patches) != 0 {
 		t.Errorf("expected 0 patches but got: %+v", patches)
 	}
@@ -102,9 +127,10 @@ func TestValidatePodWithNonDefaultAnnotation(t *testing.T) {
 	pod := corev1.Pod{}
 	var patches []string
 	var validationErrors []string
+	mutationAllowed := true
 
 	pod.Spec.ServiceAccountName = "default"
-	patches, validationErrors = admitServiceAccountToken(pod, "non-default", patches, validationErrors)
+	patches, validationErrors = admitServiceAccountToken(pod, "non-default", mutationAllowed, patches, validationErrors)
 	if len(patches) != 0 {
 		t.Errorf("expected 0 patches but got: %+v", patches)
 	}
@@ -117,9 +143,10 @@ func TestValidatePodWithNonDefaultAnnotationNonDefaultServiceAccount(t *testing.
 	pod := corev1.Pod{}
 	var patches []string
 	var validationErrors []string
+	mutationAllowed := true
 
 	pod.Spec.ServiceAccountName = "test"
-	patches, validationErrors = admitServiceAccountToken(pod, "non-default", patches, validationErrors)
+	patches, validationErrors = admitServiceAccountToken(pod, "non-default", mutationAllowed, patches, validationErrors)
 	if len(patches) != 0 {
 		t.Errorf("expected 0 patches but got: %+v", patches)
 	}
@@ -127,4 +154,3 @@ func TestValidatePodWithNonDefaultAnnotationNonDefaultServiceAccount(t *testing.
 		t.Errorf("expected 0 validationErrors but got: %+v", validationErrors)
 	}
 }
-

commit b3304a5f5658433f6a39907cadfaf881426ec469

[FIX] Fix bug for remove-default option for admission hook caused by #84
The AdmitPod function is executed twice, once for the mutating
webhook and the second time for the validating webhook. This
will always cause a validation error and the patch won't have
any effect.

pkg/admission/karydia/karydia.go
@@ -106,27 +106,24 @@ func admitServiceAccountToken(pod corev1.Pod, annotation string, mutationAllowed
 		}
 	} else if annotation == "remove-default" {
 		// Mutating webhook
-		if mutationAllowed {
 
-			if automountServiceAccountTokenUndefined(&pod) && pod.Spec.ServiceAccountName == "default" {
-				patches = append(patches, fmt.Sprintf(`{"op": "add", "path": "/spec/automountServiceAccountToken", "value": %s}`, "false"))
-				patches = append(patches, fmt.Sprintf(`{"op": "remove", "path": "/spec/serviceAccountName"}`))
-				for i, v := range pod.Spec.Volumes {
-					if strings.HasPrefix(v.Name, "default-token-") {
-						patches = append(patches, fmt.Sprintf(`{"op": "remove", "path": "/spec/volumes/%d"}`, i))
-					}
+		if automountServiceAccountTokenUndefined(&pod) && pod.Spec.ServiceAccountName == "default" {
+			patches = append(patches, fmt.Sprintf(`{"op": "add", "path": "/spec/automountServiceAccountToken", "value": %s}`, "false"))
+			patches = append(patches, fmt.Sprintf(`{"op": "remove", "path": "/spec/serviceAccountName"}`))
+			for i, v := range pod.Spec.Volumes {
+				if strings.HasPrefix(v.Name, "default-token-") {
+					patches = append(patches, fmt.Sprintf(`{"op": "remove", "path": "/spec/volumes/%d"}`, i))
 				}
-				for i, c := range pod.Spec.Containers {
-					for j, v := range c.VolumeMounts {
-						if strings.HasPrefix(v.Name, "default-token-") {
-							patches = append(patches, fmt.Sprintf(`{"op": "remove", "path": "/spec/containers/%d/volumeMounts/%d"}`, i, j))
-						}
+			}
+			for i, c := range pod.Spec.Containers {
+				for j, v := range c.VolumeMounts {
+					if strings.HasPrefix(v.Name, "default-token-") {
+						patches = append(patches, fmt.Sprintf(`{"op": "remove", "path": "/spec/containers/%d/volumeMounts/%d"}`, i, j))
 					}
 				}
 			}
-		} else {
-			validationErrors = append(validationErrors, "option 'remove-default' for  karydia.gardener.cloud/automountServiceAccountToken is only available for mutating webhooks")
 		}
+
 	}
 	return patches, validationErrors
 }

pkg/admission/karydia/service_token_test.go
@@ -41,27 +41,6 @@ func TestMutatePodWithRemoveDefaultAnnotation(t *testing.T) {
 	}
 }
 
-func TestValidatePodWithRemoveDefaultAnnotation(t *testing.T) {
-	pod := corev1.Pod{}
-	var patches []string
-	var validationErrors []string
-
-	mutationAllowed := false
-
-	pod.Spec.ServiceAccountName = "default"
-	pod.Spec.Volumes = append([]corev1.Volume{}, corev1.Volume{Name: "default-token-abcd", VolumeSource: corev1.VolumeSource{}})
-	mounts := append([]corev1.VolumeMount{}, corev1.VolumeMount{Name: "default-token-abcd"})
-	pod.Spec.Containers = append([]corev1.Container{}, corev1.Container{Name: "first-container", VolumeMounts: mounts})
-
-	patches, validationErrors = admitServiceAccountToken(pod, "remove-default", mutationAllowed, patches, validationErrors)
-	if len(patches) != 0 {
-		t.Errorf("expected 0 patches but got: %+v", patches)
-	}
-	if len(validationErrors) != 1 {
-		t.Errorf("expected 1 validationErrors but got: %+v", validationErrors)
-	}
-}
-
 func TestMutatePodWithRemoveDefaultAnnotationNonDefaultServiceAccount(t *testing.T) {
 	pod := corev1.Pod{}
 	var patches []string