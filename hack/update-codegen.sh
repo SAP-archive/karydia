839d65b8ec3b236e58dab9096702f44001d3ac05
Jonas Zengerle <33089342+ionysos@users.noreply.github.com>
2019-05-31 13:18:49 +0200

    Update codegen scripts

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -18,14 +18,21 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 
-chmod +x ${CODEGEN_PKG}/generate-groups.sh
+chmod +x "${CODEGEN_PKG}"/generate-groups.sh
 
-${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
+# generate the code with:
+# --output-base    because this script should also be able to run inside the vendor dir of
+#                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
+#                  instead of the $GOPATH directly. For normal projects this can be dropped.
+"${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
   github.com/karydia/karydia/pkg/client github.com/karydia/karydia/pkg/apis \
   karydia:v1alpha1 \
-  --go-header-file ${SCRIPT_ROOT}/hack/header.txt
+  --output-base "${SCRIPT_ROOT}"/pkg-generated/ \
+  --go-header-file "${SCRIPT_ROOT}"/hack/header.txt
 
-chmod -x ${CODEGEN_PKG}/generate-groups.sh
+chmod -x "${CODEGEN_PKG}"/generate-groups.sh
+cp -Rf "${SCRIPT_ROOT}"/pkg-generated/github.com/karydia/karydia/pkg/ "${SCRIPT_ROOT}"/pkg/
+rm -rf "${SCRIPT_ROOT}"/pkg-generated
