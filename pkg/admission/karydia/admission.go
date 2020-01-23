d3a4fc0ff127cd36515119fba880c35df884f760
Neumann-Nils <N_Neumann@outlook.de>
2020-01-23 16:58:51 +0100

    Do not admit terminating pod

--- pkg/admission/karydia/admission.go
+++ pkg/admission/karydia/admission.go
@@ -94,6 +94,11 @@ func (k *KarydiaAdmission) Admit(ar v1beta1.AdmissionReview, mutationAllowed boo
 			return k8sutil.ErrToAdmissionResponse(err)
 		}
 
+		k.logger.Infoln("POD STATUS (CALC): ", getPodStatus(pod)) 
+		if (getPodStatus(pod) == "Terminating") {
+			return k8sutil.AllowAdmissionResponse()
+		}
+
 		if mutationAllowed {
 			return k.mutatePod(pod, namespace)
 		}
@@ -154,4 +159,68 @@ func shouldIgnoreEvent(ar v1beta1.AdmissionReview) bool {
 		return true
 	}
 	return false
+}
+
+// https://github.com/kubernetes/kubernetes/blob/a38096a0696514a034de7f8d0cc5a3ec5e7da8ff/pkg/printers/internalversion/printers.go#L659-L781
+func getPodStatus(pod *v1.Pod) string {
+	reason := string(pod.Status.Phase)
+
+	initializing := false
+	for i := range pod.Status.InitContainerStatuses {
+		container := pod.Status.InitContainerStatuses[i]
+		switch {
+		case container.State.Terminated != nil && container.State.Terminated.ExitCode == 0:
+			continue
+		case container.State.Terminated != nil:
+			// initialization is failed
+			if len(container.State.Terminated.Reason) == 0 {
+				if container.State.Terminated.Signal != 0 {
+					reason = fmt.Sprintf("Init:Signal:%d", container.State.Terminated.Signal)
+				} else {
+					reason = fmt.Sprintf("Init:ExitCode:%d", container.State.Terminated.ExitCode)
+				}
+			} else {
+				reason = "Init:" + container.State.Terminated.Reason
+			}
+			initializing = true
+		case container.State.Waiting != nil && len(container.State.Waiting.Reason) > 0 && container.State.Waiting.Reason != "PodInitializing":
+			reason = "Init:" + container.State.Waiting.Reason
+			initializing = true
+		default:
+			reason = fmt.Sprintf("Init:%d/%d", i, len(pod.Spec.InitContainers))
+			initializing = true
+		}
+		break
+	}
+	if !initializing {
+		hasRunning := false
+		for i := len(pod.Status.ContainerStatuses) - 1; i >= 0; i-- {
+			container := pod.Status.ContainerStatuses[i]
+
+			if container.State.Waiting != nil && container.State.Waiting.Reason != "" {
+				reason = container.State.Waiting.Reason
+			} else if container.State.Terminated != nil && container.State.Terminated.Reason != "" {
+				reason = container.State.Terminated.Reason
+			} else if container.State.Terminated != nil && container.State.Terminated.Reason == "" {
+				if container.State.Terminated.Signal != 0 {
+					reason = fmt.Sprintf("Signal:%d", container.State.Terminated.Signal)
+				} else {
+					reason = fmt.Sprintf("ExitCode:%d", container.State.Terminated.ExitCode)
+				}
+			} else if container.Ready && container.State.Running != nil {
+				hasRunning = true
+			}
+		}
+
+		// change pod status back to "Running" if there is at least one container still reporting as "Running" status
+		if reason == "Completed" && hasRunning {
+			reason = "Running"
+		}
+	}
+
+	if pod.DeletionTimestamp != nil {
+		reason = "Terminating"
+	}
+
+	return reason
 }
